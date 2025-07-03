efc7051aefe3330d8d7d3f01fb6d6863a68fc71e
Tepathip <tepathip.pass@gmail.com>
2025-07-03 21:16:02 +0700

    delete enable cors

--- job-tracker-backend/src/main.ts
+++ job-tracker-backend/src/main.ts
@@ -5,11 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:8080', 'http://localhost:5173',],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
-  })
+  // app.enableCors({
+  //   origin: ['http://localhost:8080', 'http://localhost:5173',],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true
+  // })
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
