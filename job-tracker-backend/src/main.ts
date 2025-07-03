8e6544cfb0a1dc6086e38083bb2924cee753f11f
Tepathip <tepathip.pass@gmail.com>
2025-07-03 21:12:56 +0700

    fix enable cors

--- job-tracker-backend/src/main.ts
+++ job-tracker-backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:8080', 'http://localhost:5173', 'https://jobtracker.tepathipm.com/'],
+    origin: ['http://localhost:8080', 'http://localhost:5173',],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
   })
