f0ec9f2f48bdd22f50f86015b2003d564e7666e6
Tepathip <tepathip.pass@gmail.com>
2025-07-03 20:57:22 +0700

    allow cors for front

--- job-tracker-backend/src/main.ts
+++ job-tracker-backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:8080', 'http://localhost:5173'],
+    origin: ['http://localhost:8080', 'http://localhost:5173', 'https://jobtracker.tepathipm.com/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
   })
